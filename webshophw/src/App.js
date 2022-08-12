26e213c01b13577bd78c014dae40ec780198c823
KeeYoo <poronko2045@gmail.com>
2022-08-12 16:11:10 +0300

    V2

--- webshophw/src/App.js
+++ webshophw/src/App.js
@@ -7,6 +7,7 @@ import Home from './pages/Home';
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Bucket from './pages/Bucket';
+import Footer from './components/Footer';
 
 function App() {
 
@@ -36,6 +37,7 @@ function App() {
         <Route path="/bucket" element={<Bucket bucketProducts={cart}/>} />
         {/* <Route path="/item:id" element={<ItemDescription />} /> */}
       </Routes>
+      <Footer/>
     </>
   );
 }
--- webshophw/src/pages/Bucket.jsx
+++ webshophw/src/pages/Bucket.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 
 export default function Bucket({bucketProducts}) {
+  const price = bucketProducts.reduce((acc, product) => acc + product.price, 0)
   return (
-    <div></div>
+    <div>
+      <h1 className='center'>Bucket</h1>
+      <h1  className='center' style={{'font-size': '20px', 'margin': '20px 0'}}>Overall Price: {price}$</h1>
+      {bucketProducts.length > 0 ? bucketProducts.map(product => (
+      <div className='cart-item'>
+        <img src={product.image}/>
+        <div className='cart-desc'>
+            <h2>{product.title}</h2>
+            <h4>{product.description}</h4>
+            <b>{product.price}$</b>
+        </div>
+      </div>
+    )) : <div>Cart is empty</div>}
+    </div>
   )
 }
